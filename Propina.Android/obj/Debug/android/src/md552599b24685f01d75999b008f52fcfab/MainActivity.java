896cd81403931f4e3573eec15273bcb4efda750c
Roxana Coronel <ricoronel@misena.edu.co>
2018-08-23 14:27:15 -0500

    Initial commit

---
+++ Propina.Android/obj/Debug/android/src/md552599b24685f01d75999b008f52fcfab/MainActivity.java
@@ -0,0 +1,47 @@
+package md552599b24685f01d75999b008f52fcfab;
+
+
+public class MainActivity
+	extends md51558244f76c53b6aeda52c8a337f2c37.FormsAppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("Propina.Droid.MainActivity, Propina.Android", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("Propina.Droid.MainActivity, Propina.Android", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
---
+++ Propina/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Propina
+{
+    public partial class MainPage : ContentPage
+    {
+        void Handle_Clicked(object sender, System.EventArgs e)
+        {
+            //Conversion de variables
+            double total = double.Parse(Etotal.Text);
+            double propina =double.Parse(Epropina.Text);
+            int numeropersona = Convert.ToInt32(Enpersonas.Text);
+            double respropina = (total * propina) / 100;
+            double resTotal = total + respropina;
+            double rsProPe = respropina / numeropersona;
+            double resTolPers = resTotal / numeropersona;
+
+            //Set De Text
+
+            Tpropina.Detail = "$"+respropina;
+            Total.Detail = "$" + resTotal; 
+            Tpropinap.Detail = "$" + rsProPe;
+            Ttpersona.Detail = "$" + resTolPers;
+
+
+
+        }
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
